7e38fedff2afb8f356563deccea9dcaf327f7ecd
soban <sobanshams88@gmail.com>
2021-12-02 23:28:48 +0500

    commit 1

---
+++ src/App.js
@@ -0,0 +1,437 @@
+import React, { useEffect, useState } from "react";
+import ClipLoader from "react-spinners/ClipLoader";
+import { contract_address, contract_abi } from "./constants";
+import getWeb3 from "./getWeb3";
+import nftVideos from './exportVideos'
+
+export default function App() {
+ 
+  const [contract, setcontract] = useState("");
+  const [accounts, setaccounts] = useState();
+  const [, setNftsJson] = useState([]);
+  const [nftMetadata, setnftMetadata] = useState([]);
+  const [Loading, setLoading] = useState(false);
+  const [packages, setPackages] = useState([
+    {
+      name: "Genesis",
+      price: "0.75BNB",
+      remainingSupply: 0,
+      totalSupply: 9,
+      imgSrc: "https://hypeculture.net/storage/2021/10/genesis-1-614x1024.png",
+      srcSet: `
+    https://hypeculture.net/storage/2021/10/genesis-1-614x1024.png  614w,
+    https://hypeculture.net/storage/2021/10/genesis-1-180x300.png   180w,
+    https://hypeculture.net/storage/2021/10/genesis-1-768x1280.png  768w,
+    https://hypeculture.net/storage/2021/10/genesis-1-922x1536.png  922w,
+    https://hypeculture.net/storage/2021/10/genesis-1-600x1000.png  600w,
+    https://hypeculture.net/storage/2021/10/genesis-1.png          1200w
+  `,
+    },
+    {
+      name: "Legendary",
+      price: "0.24BNB",
+      totalSupply: 25,
+      remainingSupply: 0,
+
+      imgSrc:
+        "https://hypeculture.net/storage/2021/10/legendary-1-614x1024.png",
+      srcSet: `    
+    https://hypeculture.net/storage/2021/10/legendary-1-614x1024.png  614w,
+    https://hypeculture.net/storage/2021/10/legendary-1-180x300.png   180w,
+    https://hypeculture.net/storage/2021/10/legendary-1-768x1280.png  768w,
+    https://hypeculture.net/storage/2021/10/legendary-1-922x1536.png  922w,
+    https://hypeculture.net/storage/2021/10/legendary-1-600x1000.png  600w,
+    https://hypeculture.net/storage/2021/10/legendary-1.png          1200w
+  `,
+    },
+    {
+      name: "Mythic",
+      price: "0.49BNB",
+      totalSupply: 13,
+      remainingSupply: 0,
+      imgSrc: "https://hypeculture.net/storage/2021/10/mythic-1-614x1024.png",
+      srcSet: `  
+    https://hypeculture.net/storage/2021/10/mythic-1-614x1024.png  614w,
+    https://hypeculture.net/storage/2021/10/mythic-1-180x300.png   180w,
+    https://hypeculture.net/storage/2021/10/mythic-1-768x1280.png  768w,
+    https://hypeculture.net/storage/2021/10/mythic-1-922x1536.png  922w,
+    https://hypeculture.net/storage/2021/10/mythic-1-600x1000.png  600w,
+    https://hypeculture.net/storage/2021/10/mythic-1.png          1200w
+  `,
+    },
+    {
+      name: "Rare",
+      price: "0.12BNB",
+      totalSupply: 53,
+      remainingSupply: 0,
+      imgSrc: "https://hypeculture.net/storage/2021/10/rare-1-614x1024.png",
+      srcSet: `  
+     https://hypeculture.net/storage/2021/10/rare-1-614x1024.png  614w,
+    https://hypeculture.net/storage/2021/10/rare-1-180x300.png   180w,
+    https://hypeculture.net/storage/2021/10/rare-1-768x1280.png  768w,
+    https://hypeculture.net/storage/2021/10/rare-1-922x1536.png  922w,
+    https://hypeculture.net/storage/2021/10/rare-1-600x1000.png  600w,
+    https://hypeculture.net/storage/2021/10/rare-1.png          1200w
+  `,
+    },
+  ]);
+  const [selectedTypes, setSelectedTypes] = useState([]);
+  useEffect(() => {
+    getWeb3Custom();
+  }, []);
+
+  const getWeb3Custom = async () => {
+    const web3 = await getWeb3();
+    const accounts = await web3.eth.getAccounts();
+    const instance = new web3.eth.Contract(contract_abi, contract_address);
+    setcontract(instance);
+    setaccounts(accounts);
+    instance && accounts && getNftsByAddress(instance, accounts);
+  };
+
+  const getNftsByAddress = async (contract, accounts) => {
+    let nftsId =
+      accounts.length &&
+      (await contract?.methods?.getNftsByAddress(accounts[0]).call());
+    if (nftsId.length) {
+      const uriArray = await Promise.all(
+        nftsId.map(async (id) => {
+          let uri = await contract?.methods?.getURI(id).call();
+          const uriObj = {uri , id}
+          return uriObj;
+        })
+      );
+      setNftsJson(uriArray);
+      if (uriArray.length) {
+        const getMp4Url = await Promise.all(
+          uriArray.map(async (uri) => {
+            let uriRes = await fetch(uri.uri);
+            uriRes = await uriRes.json();
+            uriRes = { ...uriRes , uri : uri.uri  , id: uri.id};
+            return uriRes;
+          })
+        );
+        setnftMetadata(getMp4Url);
+      }
+    }
+
+    const modifiedPackage = await Promise.all(
+      packages.map(async (packageObj) => {
+        let modifiedObj = {};
+        const supply =
+          accounts.length &&
+          (await contract?.methods?.getSupplyByType(packageObj.name).call());
+        modifiedObj = {
+          ...packageObj,
+          remainingSupply: packageObj.totalSupply - supply,
+        };
+        return modifiedObj;
+      })
+    );
+    modifiedPackage.length && setPackages(modifiedPackage);
+    setLoading(false);
+  };
+  const connectToMetamask = async () => {
+    window &&
+      window.ethereum &&
+      window.ethereum.request({ method: "eth_requestAccounts" });
+  };
+
+  window &&
+    window.ethereum &&
+    window.ethereum.on("accountsChanged", function (accounts) {
+      // Time to reload your interface with accounts[0]!
+      // contract && accounts &&  getNftsByAddress(contract ,accounts)
+      window.location.reload();
+    });
+
+  const getBuyNft = async () => {
+    if (selectedTypes.length < 1) {
+      return;
+    }
+    // setLoading(true)
+    let nftsPrice = await contract.methods
+      .calculateNftPrice(selectedTypes)
+      .call();
+    if (nftsPrice < 1) {
+      return;
+    }
+     await contract.methods
+      .buyInBulk(selectedTypes)
+      .send({ from: accounts[0], value: nftsPrice });
+
+    setLoading(true);
+    getNftsByAddress(contract, accounts);
+  };
+
+  const selectPackage = (name) => {
+    if (selectedTypes.includes(name)) {
+      const filteredPackages = selectedTypes.filter((pack) => pack !== name);
+      setSelectedTypes(filteredPackages);
+    } else {
+      setSelectedTypes([...selectedTypes, name]);
+    }
+  };
+
+  if (Loading) {
+    return <ClipLoader color={"white"} loading={Loading} size={150} />;
+  }
+  return (
+    <div>
+      <div className="elementor-widget-container">
+      
+        {accounts && accounts.length ? (
+          <div>
+          <div className="elementor-widget-container">
+          <div
+            className="
+        qodef-shortcode qodef-m qodef-section-title
+        qodef-alignment--center "
+            style={{ margin: "120px 0px" }}
+          >
+            <h1 className="qodef-m-title">Choose Bundles</h1>
+          </div>
+        </div>
+            <div style={{ textAlign: "center" }}>
+              <section
+                className="
+          elementor-section
+          elementor-top-section
+          elementor-element
+          elementor-element-705fef1
+          qodef-elementor-content-grid
+          elementor-section-full_width
+          elementor-section-content-middle
+          elementor-section-height-default
+          elementor-section-height-default
+        "
+                data-id="705fef1"
+                data-element_type="section"
+              >
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                  }}
+                  className="elementor-container elementor-column-gap-no"
+                >
+                  {packages.map((Package) => {
+                    return (
+                      <div
+                        className="
+              elementor-column
+              elementor-col-285
+              elementor-top-column
+              elementor-element
+              elementor-element-35e0623
+            "
+                        data-id="35e0623"
+                        data-element_type="column"
+                        style={{ marginRight: "35px", marginBottom: "40px" }}
+                      >
+                        <div
+                          className="elementor-widget-wrap elementor-element-populated"
+                          onClick={() => selectPackage(Package.name)}
+                          style={
+                            selectedTypes.includes(Package.name)
+                              ? { border: "1px solid", cursor: "pointer" }
+                              : { cursor: "pointer" }
+                          }
+                        >
+                          <div
+                            className="
+                  elementor-element
+                  elementor-element-2ec303a
+                  elementor-widget
+                  elementor-widget-eldon_core_numbered_text
+                "
+                            data-id="2ec303a"
+                            data-element_type="widget"
+                            data-widget_type="eldon_core_numbered_text.default"
+                          >
+                            <div className="elementor-widget-container">
+                              <div
+                                className="
+                      qodef-shortcode qodef-m qodef-numbered-text
+                    "
+                              >
+                                <div className="qodef-m-content">
+                                  <h3 className="qodef-m-title">
+                                    {" "}
+                                    {Package.name}{" "}
+                                  </h3>
+                                  <h3 className="qodef-m-title">
+                                    {" "}
+                                    {Package.remainingSupply}/
+                                    {Package.totalSupply}
+                                  </h3>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                          <div
+                            className="
+                  elementor-element
+                  elementor-element-73e3105
+                  elementor-widget
+                  elementor-widget-image
+                "
+                            data-id="73e3105"
+                            data-element_type="widget"
+                            data-widget_type="image.default"
+                          >
+                            <div className="elementor-widget-container">
+                              <img
+                                width="370"
+                                height="417"
+                                src={Package.imgSrc}
+                                className="attachment-medium size-medium"
+                                alt=""
+                                srcset={Package.srcSet}
+                                sizes="(max-width: 414px) 80vw, 414px"
+                              />
+                            </div>
+                            <h3 className="qodef-m-title"> {Package.price} </h3>
+                          </div>
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              </section>
+              <div className="elementor-widget-container">
+                <input
+                  onClick={getBuyNft}
+                  type="submit"
+                  style={{ marginTop: "60px" }}
+                  name="submit"
+                  className="es_subscription_form_submit es_submit_button es_textbox_button "
+                  id="es_subscription_form_submit_61a61952eae4f"
+                  value="Buy Now"
+                />
+              </div>
+            </div>
+
+            <div style={{ textAlign: "center" }}>
+              <section
+                className="
+        elementor-section
+        elementor-top-section
+        elementor-element
+        elementor-element-705fef1
+        qodef-elementor-content-grid
+        elementor-section-full_width
+        elementor-section-content-middle
+        elementor-section-height-default
+        elementor-section-height-default
+      "
+                data-id="705fef1"
+                data-element_type="section"
+              >
+                <div className="elementor-widget-container">
+                  <div
+                    className="
+        qodef-shortcode qodef-m qodef-section-title
+        qodef-alignment--center "
+                    style={{ margin: "120px 0px" }}
+                  >
+                    <h1 className="qodef-m-title">My NFTs</h1>
+                  </div>
+                </div>
+                <div
+                  style={{
+                    display: "flex",
+                    flexWrap: "wrap",
+                    justifyContent: "center",
+                  }}
+                  className="elementor-container elementor-column-gap-no"
+                >
+                  {nftMetadata?.length ? (
+                    nftMetadata.map((metaData) => {
+                      return (
+                        <div
+                          className="
+            elementor-column
+            elementor-col-285
+            elementor-top-column
+            elementor-element
+            elementor-element-35e0623
+          "
+                          data-id="35e0623"
+                          data-element_type="column"
+                          style={{ marginRight: "35px" }}
+                        >
+                          <div
+                            className="
+              elementor-widget-wrap elementor-element-populated
+            "
+                          >
+                            <div
+                              className="
+                elementor-element
+                elementor-element-73e3105
+                elementor-widget
+                elementor-widget-image
+              "
+                              data-id="73e3105"
+                              data-element_type="widget"
+                              data-widget_type="image.default"
+                            >
+                              <div
+                                className="elementor-widget-container"
+                                style={{
+                                  display: "flex",
+                                  justifyContent: "center",
+                                }}
+                              >
+                                <video
+                                  width="370"
+                                  height="417"
+                                  src={nftVideos[`video${metaData.id}`]}
+                                  style={{ border: "1px solid" }}
+                                  autoPlay
+                                  loop
+                                />
+                              </div>
+                              <a href={metaData.uri}>
+                                {" "}
+                                <h6 className="qodef-m-title">
+                                  {" "}
+                                  {metaData.name}{" "}
+                                </h6>{" "}
+                              </a>
+                            </div>
+                          </div>
+                        </div>
+                      );
+                    })
+                  ) : (
+                    <h6 className="qodef-m-title">
+                      {" "}
+                      You have not purchased any nft :({" "}
+                    </h6>
+                  )}
+                </div>
+              </section>
+            </div>
+          </div>
+        ) : (
+          <div
+            className="elementor-widget-container"
+            style={{ marginBottom: "300px" , marginTop : "200px", textAlign: "center" }}
+          >
+            <input
+              onClick={connectToMetamask}
+              type="submit"
+              // style={{ marginTop: "60px" }}
+              name="submit"
+              className="es_subscription_form_submit es_submit_button es_textbox_button "
+              id="es_subscription_form_submit_61a61952eae4f"
+              value="Connect Metamask"
+            />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
---
+++ src/exportVideos.js
@@ -0,0 +1,175 @@
+import video1 from "./final-nfts/1.mp4";
+import video2 from "./final-nfts/2.mp4";
+import video3 from "./final-nfts/3.mp4";
+import video4 from "./final-nfts/4.mp4";
+import video5 from "./final-nfts/5.mp4";
+import video6 from "./final-nfts/6.mp4";
+import video7 from "./final-nfts/7.mp4";
+import video8 from "./final-nfts/8.mp4";
+import video9 from "./final-nfts/9.mp4";
+import video10 from "./final-nfts/10.mp4";
+// import video11 from "./final-nfts/11.mp4";
+// import video12 from "./final-nfts/12.mp4";
+// import video13 from "./final-nfts/13.mp4";
+// import video14 from "./final-nfts/14.mp4";
+// import video15 from "./final-nfts/15.mp4";
+// import video16 from "./final-nfts/16.mp4";
+// import video17 from "./final-nfts/17.mp4";
+// import video18 from "./final-nfts/18.mp4";
+// import video19 from "./final-nfts/19.mp4";
+// import video20 from "./final-nfts/20.mp4";
+// import video21 from "./final-nfts/21.mp4";
+// import video22 from "./final-nfts/22.mp4";
+// import video23 from "./final-nfts/23.mp4";
+// import video24 from "./final-nfts/24.mp4";
+// import video25 from "./final-nfts/25.mp4";
+// import video26 from "./final-nfts/26.mp4";
+// import video27 from "./final-nfts/27.mp4";
+// import video28 from "./final-nfts/28.mp4";
+// import video29 from "./final-nfts/29.mp4";
+// import video30 from "./final-nfts/30.mp4";
+// import video31 from "./final-nfts/31.mp4";
+// import video32 from "./final-nfts/32.mp4";
+// import video33 from "./final-nfts/33.mp4";
+// import video34 from "./final-nfts/34.mp4";
+// import video35 from "./final-nfts/35.mp4";
+// import video36 from "./final-nfts/36.mp4";
+// import video37 from "./final-nfts/37.mp4";
+// import video38 from "./final-nfts/38.mp4";
+// import video39 from "./final-nfts/39.mp4";
+// import video40 from "./final-nfts/40.mp4";
+// import video41 from "./final-nfts/41.mp4";
+// import video42 from "./final-nfts/42.mp4";
+// import video43 from "./final-nfts/43.mp4";
+// import video44 from "./final-nfts/44.mp4";
+// import video45 from "./final-nfts/45.mp4";
+// import video46 from "./final-nfts/46.mp4";
+// import video47 from "./final-nfts/47.mp4";
+// import video48 from "./final-nfts/48.mp4";
+// import video49 from "./final-nfts/49.mp4";
+// import video50 from "./final-nfts/50.mp4";
+// import video51 from "./final-nfts/51.mp4";
+// import video52 from "./final-nfts/52.mp4";
+// import video53 from "./final-nfts/53.mp4";
+// import video54 from "./final-nfts/54.mp4";
+// import video55 from "./final-nfts/55.mp4";
+// import video56 from "./final-nfts/56.mp4";
+// import video57 from "./final-nfts/57.mp4";
+// import video58 from "./final-nfts/58.mp4";
+// import video59 from "./final-nfts/59.mp4";
+// import video60 from "./final-nfts/60.mp4";
+// import video61 from "./final-nfts/61.mp4";
+// import video62 from "./final-nfts/62.mp4";
+// import video63 from "./final-nfts/63.mp4";
+// import video64 from "./final-nfts/64.mp4";
+// import video65 from "./final-nfts/65.mp4";
+// import video66 from "./final-nfts/66.mp4";
+// import video67 from "./final-nfts/67.mp4";
+// import video68 from "./final-nfts/68.mp4";
+// import video69 from "./final-nfts/69.mp4";
+// import video70 from "./final-nfts/70.mp4";
+// import video71 from "./final-nfts/71.mp4";
+// import video72 from "./final-nfts/72.mp4";
+// import video73 from "./final-nfts/73.mp4";
+// import video74 from "./final-nfts/74.mp4";
+// import video75 from "./final-nfts/75.mp4";
+// import video76 from "./final-nfts/76.mp4";
+// import video77 from "./final-nfts/77.mp4";
+// import video78 from "./final-nfts/78.mp4";
+// import video79 from "./final-nfts/79.mp4";
+// import video80 from "./final-nfts/80.mp4";
+// import video81 from "./final-nfts/81.mp4";
+// import video82 from "./final-nfts/82.mp4";
+// import video83 from "./final-nfts/83.mp4";
+// import video84 from "./final-nfts/84.mp4";
+// import video85 from "./final-nfts/85.mp4";
+
+const videos = {
+  video1,
+  video2,
+  video3,
+  video4,
+  video5,
+  video6,
+  video7,
+  video8,
+  video9,
+  video10,
+//   video11,
+//   video12,
+//   video13,
+//   video14,
+//   video15,
+//   video16,
+//   video17,
+//   video18,
+//   video19,
+//   video20,
+//   video21,
+//   video22,
+//   video23,
+//   video24,
+//   video25,
+//   video26,
+//   video27,
+//   video28,
+//   video29,
+//   video30,
+//   video31,
+//   video32,
+//   video33,
+//   video34,
+//   video35,
+//   video36,
+//   video37,
+//   video38,
+//   video39,
+//   video40,
+//   video41,
+//   video42,
+//   video43,
+//   video44,
+//   video45,
+//   video46,
+//   video47,
+//   video48,
+//   video49,
+//   video50,
+//   video51,
+//   video52,
+//   video53,
+//   video54,
+//   video55,
+//   video56,
+//   video57,
+//   video58,
+//   video59,
+//   video60,
+//   video61,
+//   video62,
+//   video63,
+//   video64,
+//   video65,
+//   video66,
+//   video67,
+//   video68,
+//   video69,
+//   video70,
+//   video71,
+//   video72,
+//   video73,
+//   video74,
+//   video75,
+//   video76,
+//   video77,
+//   video78,
+//   video79,
+//   video80,
+//   video81,
+//   video82,
+//   video83,
+//   video84,
+//   video85,
+};
+
+export default videos;
